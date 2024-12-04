a76ce1a172a625c5dec39ad3408b15b2b3536ee3
ManojNambiraj <postboxmano@gmail.com>
2024-12-04 19:33:04 +0530

    Statements and User Inputs

---
+++ Class 5/Statements.py
@@ -0,0 +1,81 @@
+"""
+    1. Simple Statements
+    2. Complex Statements
+    3. Empty Statements
+
+"""
+
+# Control Statements
+  
+#   1) Conditional statments
+#   2) Looping statements
+
+# Strings Replication
+
+# txt = "sorry"
+
+# print(txt*1000)
+
+#   1) Conditional statments  -> comparision op -> boolean
+
+a = 8
+
+# if a > 10:
+#     print("It's greater than 10")
+# elif a == 5:
+#     print("It's equal")
+# elif a != 5:
+#     print()
+# else:
+#     print("It's lessthan 10")
+
+# Nested if
+
+# a = 10
+
+# if(a > 5):
+#     if(a == 5):
+#         print("It's True")
+#     else:
+#         print("nested wrong")
+# else:
+#     print("Something went wrong")
+
+# Short Hand if
+
+# a = 10
+
+# if a > 5: print("a is greaterthan 5")
+
+# Short Hand if...else
+
+# a = 2
+
+# print("a is greaterthan 5") if a > 5 else print("lesser")
+
+# a = 5
+
+# if a > 5 and a == 5:
+#     pass
+# else:
+#     print("The answer is wrong")
+
+# 89 find the 10th digit number?
+
+# a = 589
+
+# a //= 10
+
+# result = a % 10
+
+# print(result)
+
+# User input
+
+x = int(input("Enter the input number:"))
+
+x //= 10
+
+x %= 10
+
+print(x)
